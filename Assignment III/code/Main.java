66f6f227e1510e7b82c0f94e5d921cc11bb2e9f4
derjedi <benjamin-schroeder@outlook.com>
2015-11-01 19:53:32 +0100

    mein shit

---
+++ Assignment III/code/Main.java
@@ -0,0 +1,13 @@
+package Ex1;
+
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		parse= new Parser("C:\Users\Benjamin\Documents\GitHub\BioinformaticsI-Assigments\Assignment III\Material\output.psl");
+		
+		}
+	}
+}
---
+++ Assignment III/script_R.R
@@ -0,0 +1,1 @@
+output <-  read.table("C:/Users/Benjamin/Documents/GitHub/BioinformaticsI-Assigments/Assignment III/Material/output.psl", header=TRUE, sep="\t")

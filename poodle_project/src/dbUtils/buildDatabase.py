6fc72864e6594685e3a038cdd63e17d71e374fc7
Jonas <ol-j-d@gmx.de>
2015-11-22 10:44:39 +0100

    kleinere veränderungen  => primary key eingefügt  => cloning_vectors.csv verändert

--- poodle_project/src/dbUtils/buildDatabase.py
+++ poodle_project/src/dbUtils/buildDatabase.py
@@ -8,7 +8,7 @@ def createProtConTable(db):
     # drop table if exists
     db.execute('drop table if exists proteinConstructs')
     # create table proteinConstructs
-    db.execute('CREATE table proteinConstructs (id,box,position,protein,domain,mutation,vector,additional_number,AB,cloning_sites_QC_RF,additional_Cloning_site_behind_protein,date,author,type_of_enzyme,sequence,notes,concentration,DH5A_stock,Cplus_stock)')
+    db.execute('CREATE table proteinConstructs (id Primary Key,box,position,protein,domain,mutation,vector,additional_number,AB,cloning_sites_QC_RF,additional_Cloning_site_behind_protein,date,author,type_of_enzyme,sequence,notes,concentration,DH5A_stock,Cplus_stock)')
     values = []
     protConFile = open('../../data/protein_constructs/protein_constructs.csv','r')
     for line in protConFile.readlines():
@@ -29,7 +29,7 @@ def createClonVecTable(db):
     # drop table, if it already exists
     db.execute('drop table if exists cloningVectors')
     # create table cloningVectors
-    db.execute('create table cloningVectors (Id,Box,position,vector,size_bp,tags,TEV_cleavage,MCS,cloning_site,cloning_site_behind_protein,AB,fw_seq_primer,rv_seq_primer,protein,vector_map,date,author,concentration,DH5A_stock,notes)')
+    db.execute('create table cloningVectors (Id Primary Key,Box,position,vector,size_bp,tags,TEV_cleavage,MCS,cloning_site,cloning_site_behind_protein,AB,fw_seq_primer,rv_seq_primer,protein,vector_map,date,author,concentration,DH5A_stock,notes)')
     values = []
     clonVecFile = open('../../data/cloning_vectors.csv', 'r')
     for line in clonVecFile.readlines():
@@ -50,7 +50,7 @@ def createPrimTable(db):
     # drop table, if already exists
     db.execute('drop table if exists primer')
     # create table primer
-    db.execute('create table primer (Id,box,position,primer_name,protein,domain,restriction_QC_or_RF,restriction_site_or_mutation,primer_sequence,protein_sequence,notes_additional_informations_specification,primer_group,melting_temperature,Concentration_uM,date,name)')
+    db.execute('create table primer (Id Primary Key,box,position,primer_name,protein,domain,restriction_QC_or_RF,restriction_site_or_mutation,primer_sequence,protein_sequence,notes_additional_informations_specification,primer_group,melting_temperature,Concentration_uM,date,name)')
     values = []
     primerFile = open('../../data/primer_database.csv', 'r')
     for line in primerFile.readlines():
